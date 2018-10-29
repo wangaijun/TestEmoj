6e2d702c0733168299eaf7ff8d9fc546ae27969b
waj <wangaj@glodon.com>
2018-10-29 10:18:08 +0800

    Initial commit

---
+++ app/src/main/java/com/waj/testemoj/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.waj.testemoj
+
+import android.app.Activity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            et2.text = et1.text
+        }
+    }
+}
